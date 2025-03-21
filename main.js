d0e09ed3ed5b028e52a7f10f78e37618fe6ae0f2
Filipp Kosenko <filippkosgmc@gmail.com>
2025-03-21 15:42:34 +0200

    Changed main links

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
-        window.location.href = "../home/home.html";
+        window.location.href = "home/home.html";
     }, 4000);
 });
